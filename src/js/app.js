28e8452bf4d8b4a61f4475b917ed865c83620998
Jakub Gasiorski <jgasiorski@motoinsight.com>
2021-05-05 13:58:22 +0200

    change color of menu when scroll down

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,34 @@ import '../scss/app.scss';
 
 /* Your JS Code goes here */
 
+function OnFirstLoad() {
+    if (document.attachEvent) {
+        document.attachEvent('onscroll', scrollEvent);
+    } else if (document.addEventListener) {
+        document.addEventListener('scroll', scrollEvent, false);
+    }
+
+}
+
+$(document).ready(function () {
+    OnFirstLoad();
+});
+
+function scrollEvent(e) {
+    var body = document.body
+    var html = document.documentElement;
+
+    var docHeight = Math.max(body.scrollHeight, body.offsetHeight,
+                           html.clientHeight, html.scrollHeight, html.offsetHeight);
+    var currentScroll = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    // implement your logic according to requirement
+    if (currentScroll > 1000) {
+        // change color here navbar a
+        $('.nav-link').css('color', 'white');
+    } else {
+        $('.nav-link').css('color', 'black');
+    }
+}
 
 
 /* Demo JS */

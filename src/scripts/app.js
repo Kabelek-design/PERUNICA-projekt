4f74ea9f32a7677a0ae2354797adce13945d6f99
Kabelek-design <78099962+Kabelek-design@users.noreply.github.com>
2021-05-27 22:17:33 +0200

    button działa ale nawigacja na mobile się popsuła

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -105,20 +105,6 @@ function scrollEvent(e) {
 
 
 
-function myFunction(showHide) {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
-  }
+
 /* Demo JS */
 import './demo.js';
